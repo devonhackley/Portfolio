cd950e136a9bb3864f4c9de93d60f175dc1fb72a
devonhackley <devonhackley@gmail.com>
2016-11-30 10:11:09 -0800

    created the necessary js files and created basic template for my projects

---
+++ script/myProjects.js
@@ -0,0 +1,28 @@
+var myProjects = [
+  {
+    name: 'Pokemon Ipsum',
+    url: 'https://github.com/devonhackley',
+    info: '<p class="p-color"><img src="http://www.pokemon-sunmoon.com/media/uploads/aug_11_assets/a-raichu.png">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+
+  },
+
+  {
+    name: 'Marvel Ipsum',
+    url: 'https://github.com/devonhackley',
+    info: '<p class="p-color"><img src="http://mhsitiger.com/wp-content/uploads/2016/10/tmg-article_default_mobile.jpg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+
+  },
+  {
+    name: 'DC Ipsum',
+    url: 'https://github.com/devonhackley',
+    info: '<p class="p-color"><img src="http://www.writeups.org/wp-content/uploads/Aqualad-Young-Justice-Cartoon-Kaldurahm-d.jpg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+
+  },
+  {
+    name: 'Naruto Ipsum',
+    url: 'https://github.com/devonhackley',
+    info: '<p class="p-color"><img src="http://images.techtimes.com/data/images/full/265071/a-younger-naruto-png.png?w=760">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+
+  },
+
+];
