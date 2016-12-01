72ca6b4e32ad5ec073a6e63039c15cc767b6065d
devonhackley <devonhackley@gmail.com>
2016-12-01 10:19:07 -0800

    added a new js file and started buiilding the single page feature

--- script/myProjects.js
+++ script/myProjects.js
@@ -13,10 +13,10 @@ var myProjects = [
 
   },
   {
-    name: 'DC Ipsum',
+    name: 'Avatar Ipsum',
     url: 'https://github.com/devonhackley',
 
-    info: '<p class="p-color"><img src="http://www.writeups.org/wp-content/uploads/Aqualad-Young-Justice-Cartoon-Kaldurahm-d.jpg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+    info: '<p class="p-color"><img src="http://images.huffingtonpost.com/2014-09-06-korra.jpg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
   },
   {
     name: 'Naruto Ipsum',
---
+++ script/portfolioView.js
@@ -0,0 +1,12 @@
+var portfolioView = {};
+
+portfolioView.handleNavBar = function() {
+  $('#site-nav').on('click', '.site-nav', function() {
+    $('.content').hide();
+    $('#'+ $(this).data('content')).fadeIn('slow');
+    $('#' + $(this).data('content')).fadeIn('fast');
+  });
+  $('#site-nav .site-nav:first').click();
+};
+
+portfolioView.handleNavBar();
